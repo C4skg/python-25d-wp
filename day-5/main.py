326aef496e6a7d7263fadd426eea4ccca8bf1df5
C4skg <2269498386@qq.com>
2023-04-05 11:26:48 +0800

    submit

--- day-5/main.py
+++ day-5/main.py
@@ -1,14 +1,21 @@
 from PIL import Image
-import cv2
+import tqdm
 import os
 
 SIZE = {
     'iphone5': (1136,640)
 }
 
-for e in os.listdir('pic'):
+if not os.access('resize',os.F_OK):
+    os.mkdir('resize')
+
+dirList = os.listdir('pic');
+
+bar = tqdm.tqdm(total=len(dirList))
+
+for e in dirList:
     if e.endswith(('png','jpg')):
-        path  = f'pic/{e}'
-        image = Image.open(path)
+        image = Image.open(f'pic/{e}')
         image = image.resize(SIZE['iphone5'])
-        image.save(path)
+        image.save(f'resize/{e}')
+        bar.update(1);
