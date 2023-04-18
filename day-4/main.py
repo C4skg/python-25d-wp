62dde7f212dd2a874599802e5f58800338f26ffa
C4skg <2269498386@qq.com>
2023-04-18 10:39:05 +0800

    Update main.py

--- day-4/main.py
+++ day-4/main.py
@@ -1,8 +1,7 @@
 import re
-import sys
 import time
 
-comp = re.compile(r'([a-z]{1})',re.I);
+comp = re.compile(r'([a-z\']+)',re.I);
 
 mode = int(input('请输入1或2来选择模式:').strip(''));
 
@@ -10,7 +9,7 @@ if mode == 1:
     userIn = input('请输入一段文本以统计单词数:');
     print(f'文中单词数:{len(comp.findall(userIn))}')
 else:
-    file = input('请输入待读取文件路径：');
+    file = input('请输入待读取文件路径：'); 
     start = time.time();
     with open(f'{file}',"r",encoding='utf-8') as f:
         print(f'文中单词数:{len(comp.findall(f.read()))}')
